3b5520aa35cbe62492ffdeab5d1a78f9751fe72a
mahkumar13 <getmahi25@gmail.com>
2021-05-26 20:09:12 +0530

    first commit

---
+++ StudentManager/StudentManager/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StudentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("No of students in class?");
+            var studentCount = int.Parse(Console.ReadLine());
+            var studentGrades = new int[studentCount];
+            var studentNames = new string[studentCount];
+            for (int i=0;i<studentCount;i++)
+            {
+                Console.Write("student name:");
+                studentNames[i] = Console.ReadLine();
+
+                Console.Write("student grade");
+                studentGrades[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Name: {0},Grade: {1}", studentNames[i], studentGrades[i]);
+            }
+        }
+    }
+}
